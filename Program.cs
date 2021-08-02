72e45ede862bfac77e23053e05a1d703c6e7fe7a
shreyaaku@outlook.com <shreyaaku@outlook.com>
2021-08-02 10:39:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EX1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// my name
+            Console.WriteLine("Shreya A");
+            Console.WriteLine();
+            ///Nemesis
+            Console.WriteLine("LOL");
+        }
+    }
+}

3b37fd8b5b46ced857133f7f2bfd0799fb350a22
shreyaaaaaaaaaaaaaaa <87992282+shreyaaaaaaaaaaaaaaa@users.noreply.github.com>
2021-08-02 18:28:44 +0530

    Add files via upload

---
+++ substringsprogram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace substrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stg = "ABCD";
+            int initiallength = 1;
+            int firstindex = 0;
+            int len = stg.Length;
+            for (initiallength = 1; initiallength <= len; initiallength++)
+            {
+                for (firstindex = 0; firstindex <= len - initiallength; firstindex++)
+                {
+                    string sub = stg.Substring(firstindex, initiallength);
+                    Console.WriteLine(sub);
+                }
+            }
+
+        }
+    }
+}

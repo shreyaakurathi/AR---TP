6d6d44f44719d3911f899d48834423b94e73cad5
shreyaaaaaaaaaaaaaaa <87992282+shreyaaaaaaaaaaaaaaa@users.noreply.github.com>
2021-08-11 23:46:39 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-namespace que
+namespace dies
 {
+    public class die
+    {
+        int noofsides = 6;
+    }
+    public class randomnumber
+    {
+        int random;
+        public int Roll()
+        {
+            Random random = new Random();
+            int number = random.Next(1, 7);
+            return number;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int d1, d2;
+            randomnumber random = new randomnumber();
+            d1 = random.Roll();
+            d2 = random.Roll();
+            Console.WriteLine(d1 +" "+d2);
+            int sum = d1 + d2;
+            Console.WriteLine("sum=" + sum);
         }
     }
 }
